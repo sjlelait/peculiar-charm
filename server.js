847dfe80b571ab15c318137c2351ff561bfe8fee
Sara Le Lait <sjlelait@gmail.com>
2023-01-25 18:23:12 -0700

    wip: initial setup with express, server.js & package.json

---
+++ server.js
@@ -0,0 +1,35 @@
+// require dependencies
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+require("dotenv").config();
+
+// initialize application
+
+// configure settings
+
+// configure database
+
+// mount middleware
+
+// mount routes
+
+    // SEED 
+
+    // INDEX
+
+    // NEW
+
+    // DELETE
+
+    // UPDATE
+
+    // CREATE
+
+    // EDIT
+
+    // SHOW
+
+// tell app to listen on dedicated port
+
+app.listen(PORT, () => console.log(`Express is listening on port: ${PORT}`));
