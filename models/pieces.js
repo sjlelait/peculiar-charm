fe40ac10de21dd33ce1470c528cb42432f185358
Sara Le Lait <sjlelait@gmail.com>
2023-02-04 12:16:02 -0700

    wip: works on css & adds buy link option

--- models/pieces.js
+++ models/pieces.js
@@ -5,6 +5,7 @@ const pieceSchema = new Schema({
     name: { type: String, required: true },
     description: String,
     img: { type: String, required: true },
+    link: String,
     collectionId: [{ type: Schema.Types.ObjectId, ref: "Collection"}]
 });
 module.exports = mongoose.model("Piece", pieceSchema);
